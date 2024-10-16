aaf87384a9465585ad7afeb226e2841cfa649d4f
r-nakajun <nakajunLearn@outlook.jp>
2024-10-17 07:32:00 +0900

    DBをSQLiteからPostgreSQLに変更したのでSQLiteの資材をworkフォルダに移動した。 localhost/formにアクセスした場合、404、500ページのエラーが発生して画面遷移できなかった。 トレースから解消のヒントを得るべきだったが、繰り返してコメントアウトを行い、問題無く動いた。 エラーの原因は、form.htmlの以下が原因のようなので一旦コメントアウトした。 <label for="address">住所：</label> <input type="text" id="addressId" th:field="*{address}"><br>

--- demo/src/main/java/com/example/demo/repositorys/NameDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NameDataRepository<NameData> extends JpaRepository<NameData, Long> {
-
-}
