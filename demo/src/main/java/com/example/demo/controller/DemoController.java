dcadf1bd0013d09fce35779c3b441c4125ae61d1
r-nakajun <nakajunLearn@outlook.jp>
2024-11-18 06:35:45 +0900

    バックアップのためpushです。利用者情報に住所、生年月日を追加中。

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,13 @@
 package com.example.demo.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -34,20 +41,41 @@ public class DemoController {
                 .queryForList("SELECT prefcode , prefname FROM public.prefmastertbl");
         Map<String, String> itemKindMap = new LinkedHashMap<String, String>();
         for (var prefPair : prefList) {
-            itemKindMap.put ((String) prefPair.get("prefcode"), (String) prefPair.get("prefname"));
+            itemKindMap.put((String) prefPair.get("prefcode"), (String) prefPair.get("prefname"));
         }
         model.addAttribute("itemKindMap", itemKindMap);
         model.addAttribute("user", new User());
+
+        List<String> dateList = new LinkedList<>(); 
+        LocalDate strDay = LocalDate.of(1900, 1, 1);
+        LocalDate endDay = LocalDate.of(2020,12,31);
+        long localDiffDays1 = ChronoUnit.DAYS.between(strDay, endDay);
+
+        for (int i = 0; i < Math.toIntExact(localDiffDays1); i++){
+            dateList.add(strDay.plusDays(i).toString());
+        }
+
+        // 文字列の分割
         
+
         // 生年月日を設定するプルダウンリストを作成する。
-		int birthYear = 2002;
-		int birthMonth = 8;
-		int birthDay = 3;
-		model.addAttribute("birthYear", birthYear);
-		model.addAttribute("birthMonth", birthMonth);
-		model.addAttribute("birthDay", birthDay);
+        int currentYear = YearMonth.now().getYear();
+        // 年プルダウン
+        List<String> birthYearList = new ArrayList<>();
+        for (int i = currentYear-70; i <= currentYear-20; i++) {
+            birthYearList.add(String.valueOf(i));
+        }
+        // 月プルダウン
+        List<String> monthList = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            monthList.add(String.valueOf(i));
+        }
+        // 初期値
+        String birthYearDefault = String.valueOf(currentYear-30);
 
-
+        model.addAttribute("birthYearList", birthYearList);
+        model.addAttribute("birthYearDefault", birthYearDefault);
+        model.addAttribute("monthList", monthList);
         return "form";
     }
 
